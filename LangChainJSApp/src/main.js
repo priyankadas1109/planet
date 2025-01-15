daa64f5c26a4f74b37510ea72a587be0bc79b74f
Dhananjay Surti <djsurti3003@gmail.com>
2025-01-15 14:38:22 -0800

    Follow the tutorial little by little

--- LangChainJSApp/src/main.js
+++ LangChainJSApp/src/main.js
@@ -1,8 +1,10 @@
 import { OpenAIEmbeddings, ChatOpenAI } from "@langchain/openai";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
-import { PromptTemplate } from "@langchain/core/prompts";
+import { ChatPromptTemplate, PromptTemplate } from "@langchain/core/prompts";
 import { RunnableSequence } from "@langchain/core/runnables";
 import { GithubRepoLoader } from "@langchain/community/document_loaders/web/github";
+import { Annotation, StateGraph } from "@langchain/langgraph";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
 let files = [];
 
@@ -35,6 +37,14 @@ export async function pullFromRepo(){
     const githubToken = document.getElementById('githubToken').value;
     const repoUrl = document.getElementById('sourceUrl').value;
     const apiKey = document.getElementById('apiKey').value;
+    const llm = new ChatOpenAI({
+        model: "gpt-4o-mini",
+        temperature: 0
+    });
+    const embeddings = new OpenAIEmbeddings({
+        model: "text-embedding-3-large"
+    });
+    const vectorStore = new MemoryVectorStore(embeddings);
     try{
         const response = await fetch("http://localhost:8080/api/load-github-repo", {
             method: "POST",
@@ -49,26 +59,31 @@ export async function pullFromRepo(){
         if(!response.ok){
             throw new Error(`Failed to load Github repo: ${response.statusText}`);
         }
-        const { documents } = await response.json();
-        console.log("Loaded documents:", documents);
+        const { docs } = await response.json();
+        console.log("Loaded documents:", docs);
+        const splitter = new RecursiveCharacterTextSplitter({
+            chunkSize:1000,
+            chunkOverlap:200
+        });
+        const allSplits = await splitter.splitDocuments(docs);
+
+        await vectorStore.addDocuments(allSplits);
+        const promptTemplate = await pull<ChatPromptTemplate>("rlm/rag-prompt");
+
+        //Define the state of the application
+        const InputStateAnnotation = Annotation.Root({
+            question: Annotation<string>,
+        });
+
+        const StateAnnotation = Annotation.Root({
+            question: Annotation<string>,
+            context: Annotation<Document[]>,
+            answer: Annotation<string>,
+        });
+
     } catch(e){
         console.log("Error loading documents:", e);
     }
-    // const loader = new GithubRepoLoader(
-    //     "https://github.com/djsurt/testRepo",
-    //     {
-    //       branch: "main",
-    //       recursive: true,
-    //       processSubmodules: true,
-    //       unknown: "warn",
-    //       accessToken: githubToken,
-    //       maxConcurrency: 2,
-    //     }
-    // );
-    
-    // const docs = await loader.load();
-    
-    // console.log({ docs });
     // try{
     //     await fetchFilesFromRepo(owner, repo, "", githubToken);
     //     console.log("Files:", files);
@@ -78,13 +93,6 @@ export async function pullFromRepo(){
     // at this point, we have all the files from the repo. Now we need to feed it to the RAG model
     
     // const apiKey = document.getElementById('apiKey').value;
-    // const llm = new ChatOpenAI({
-    //     model: "gpt-4o-mini",
-    //     temperature: 0
-    // });
-    // const embeddings = new OpenAIEmbeddings({
-    //     model: "text-embedding-3-large"
-    // });
-    // const vectorStore = new MemoryVectorStore(embeddings);
+
 
 }
